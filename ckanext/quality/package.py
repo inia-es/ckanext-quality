b0b0196b1b7dcd32eb69371379e6daf2d519cd9d
root <fernando.bac03@hotmail.com>
2017-07-10 01:21:23 +0200

    add quality metadata

--- ckanext/quality/package.py
+++ ckanext/quality/package.py
@@ -879,7 +879,7 @@ class PackageController(base.BaseController):
 		if (metadata):
 		    metadatosingresados=metadatosingresados+1
 	    #data_dict['quality'] = (metadatosingresados/total)*100	
-            data_dict.update({'quality': 'testtest'})
+            data_dict.update({'quality': u'testtest'})
 	    data_dict['notes'] = data_dict
 	  
 
